3af2defb1c723bd7cc77d4c9553b9dc4546c16cf
Natasha Barbosa <n.natasha.barbosa@gmail.com>
2018-08-13 17:09:44 -0300

    listagem na home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\post;
+use DB;
+
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    
 
     /**
      * Show the application dashboard.
@@ -23,6 +18,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //$post = post::all();
+        $post = DB::table('posts')->join('Users','users.id','=','posts.id_autor')->select('posts.*','Users.name')->paginate(2);
+
+        return view('home',compact('post'));
+       
+        
     }
+
+   
 }
