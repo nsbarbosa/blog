2b5d4c744bfc51fe2a1ef64749188859744a4bfa
Natasha Barbosa <n.natasha.barbosa@gmail.com>
2018-08-13 17:12:17 -0300

    rota home

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('post', 'PostController');
