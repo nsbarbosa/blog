fc21b440e6ba5eb1a3318cb3908a8f3ceb39a9eb
Natasha Barbosa <n.natasha.barbosa@gmail.com>
2018-08-15 08:02:44 -0300

    algolia scout

--- app/post.php
+++ app/post.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class post extends Model
 {
     //
+    use Searchable;
+
     protected $fillable = [ 
         'slug', 'titulo', 'descricao','id',
     ];
@@ -24,5 +27,16 @@ class post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function toSearchableArray()
+    {
+      /**
+       * Load the categories relation so that it's available
+       *  in the laravel toArray method
+       */
+      $this->categories;
+    
+      return $this->toArray();
     }
 }
